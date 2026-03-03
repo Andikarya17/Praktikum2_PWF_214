1718038310513585590410c30171655d486c66ef
Andikarya17 <andikaryaputra17@gmail.com>
2026-03-03 19:18:28 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            About Me
+        </h2>
+    </x-slot>
+
+    <div class="py-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white shadow-md rounded-lg p-6">
+                <h3 class="text-lg font-bold mb-4">Biodata</h3>
+
+                <p><strong>Nama:</strong> Andika Arya Putra</p>
+                <p><strong>NIM:</strong> 20230140214</p>
+                <p><strong>Program Studi:</strong> Informatika</p>
+                <p><strong>Hobi:</strong> Coding, Gaming, Membaca</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
